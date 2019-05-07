1f79879f68757ac3d7878ec1e966c2d973ff9589
Anthony Smith <mrpresidentsmith@gmail.com>
2019-05-07 12:39:50 -0700

    Problem 1 completed.

---
+++ Exercise4/Kid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise4
+{
+    class Kid
+    {
+
+        #region METHODS
+
+        public virtual void PrintMessage()
+        {
+            Console.WriteLine("This is a message from the Kid class.");
+        }
+
+        #endregion
+
+    }
+}
---
+++ Exercise4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kid kid = new Kid();
+            kid.PrintMessage();
+        }
+    }
+}
